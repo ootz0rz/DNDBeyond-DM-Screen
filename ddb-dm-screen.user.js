89f93056fa56924d05040edec5f57c50f16f4e17
Hardeep <h@rdeep.me>
2021-12-25 16:27:13 -0500

    add exclusion for pages like join a campaign, we only want the campaign page itself to show the stats thing at top

--- ddb-dm-screen.user.js
+++ ddb-dm-screen.user.js
@@ -5,6 +5,7 @@
 // @description     GM screen for D&DBeyond campaigns
 // @author          ootz0rz
 // @match           https://www.dndbeyond.com/campaigns/*
+// @exclude         /^https://www.dndbeyond.com/campaigns/.*?/.*?$/
 // @updateURL       https://github.com/ootz0rz/DNDBeyond-DM-Screen/raw/master/ddb-dm-screen.user.js
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @require         https://media.dndbeyond.com/character-tools/vendors~characterTools.bundle.dec3c041829e401e5940.min.js
